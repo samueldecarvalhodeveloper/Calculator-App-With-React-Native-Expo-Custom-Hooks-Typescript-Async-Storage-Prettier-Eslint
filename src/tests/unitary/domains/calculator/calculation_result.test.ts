3e5cacd89e0641fd1062279e74a826234de3e905
Samuel de Carvalho <93744509+samueldecarvalhodeveloper@users.noreply.github.com>
2024-04-26 19:04:05 -0300

    Calculation Result Class Tests Updated: Grammatical Error Fixed

--- src/tests/unitary/domains/calculator/calculation_result.test.ts
+++ src/tests/unitary/domains/calculator/calculation_result.test.ts
@@ -3,7 +3,7 @@ import CalculationResult from "../../../../domains/calculator/calculation_result
 import { EMPTY_STRING } from "../../../../constants/strings_utilities_constants";
 
 describe('Test Class: "CalculationResult"; Behavior', () => {
-  test("Test If Class Represent How Calculation Result Value Show Be Accessed Correctly", () => {
+  test("Test If Class Represent How Calculation Result Value Should Be Accessed Correctly", () => {
     const calculationResult: CalculationResult = new CalculationResult(
       EMPTY_STRING,
     );
